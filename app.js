2a13a7a2eb409dc8452bc921c866f7359b806eb7
Brian Lai <laivbrian@gmail.com>
2022-06-10 15:41:16 -0500

    Edit CPU random play decision function

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-/* randomly return rock, paper or scissors for CPU decision
+console.log("Welcome to the Rock Paper Scissors Game");
+console.log("You're against the CPU - What do you choose?");
+console.log("Type in your choice: ");
+
+/* 
     0 - rock
     1 - paper
     2 - scissors
@@ -6,7 +10,21 @@
 
 function computerPlay() {
     let cpuChoice = Math.floor(Math.random() * 3);
-    console.log(cpuChoice);
+
+    if (cpuChoice === 0) {
+        return "rock";
+    } else if (cpuChoice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
-computerPlay();
+console.log(computerPlay());
+
+// function playRound(playerSelection, computerSelection) {
+
+// }
+// const playerSelection = "rock";
+// const computerSelection = computerPlay();
+
